02fe0461744120a0b7b5f619b57b86db08fe697a
David Pellegrini <dap@davidpellegrini.com>
2021-02-19 21:18:12 -0500

    Adds comments

--- app/src/main/java/ca/davidpellegrini/w21tipcalculator/MainActivity.java
+++ app/src/main/java/ca/davidpellegrini/w21tipcalculator/MainActivity.java
@@ -17,9 +17,10 @@ import android.widget.Toast;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity
-    implements TextView.OnEditorActionListener, View.OnClickListener, SeekBar.OnSeekBarChangeListener,
-        View.OnKeyListener, RadioGroup.OnCheckedChangeListener {
+    implements TextView.OnEditorActionListener, View.OnClickListener,
+        SeekBar.OnSeekBarChangeListener, View.OnKeyListener, RadioGroup.OnCheckedChangeListener {
 
+    //sets up member variables that will be used in multiple methods
     private Button decButton, incButton;
     private TextView tipPercentTextView;
     private EditText billAmountEditText;
@@ -64,6 +65,7 @@ public class MainActivity extends AppCompatActivity
             ...
          */
 
+        // set up the tipPercent TextView so we can change it
         tipPercent = 0.2f;
         tipPercentTextView = findViewById(R.id.tipPercent);
         tipPercentTextView.setText(percent.format(tipPercent));
@@ -80,37 +82,36 @@ public class MainActivity extends AppCompatActivity
         //1. get text from EditText - convert to float
         //x. get the tip percentage (already stored in the default)
         //2. multiply the bill amount by the tip percentage
-
-        // things left
-            // seek bar
-            //x      change the tip % using the seek bar
-            //xx      update seekbar with buttons AND arrow keys**
-            //x close the keyboard when we press enter
-            //x radio buttons
-            // update as we type
-            //x hide total per person if there is only 1
-            // styles
+        //3. add the tip amount to the bill amount
+        //4. update the Views with the correct information
 
 
+        //set up the EditText so we can get the values from it
         billAmountEditText = findViewById(R.id.billAmountEditText);
-        billAmountEditText.setOnEditorActionListener(this);
-        billAmountEditText.setOnKeyListener(this);
+        billAmountEditText.setOnEditorActionListener(this); // for when we press "done" or the Action button
+        billAmountEditText.setOnKeyListener(this); // for when we pres Enter
 
+        // add event listeners to the Buttons
         decButton = findViewById(R.id.tipPercentDec);
         decButton.setOnClickListener(this);
         incButton = findViewById(R.id.tipPercentInc);
         incButton.setOnClickListener(this);
 
+        // add an event listener to the SeekBar
         percentBar = findViewById(R.id.percentSeekBar);
         percentBar.setOnSeekBarChangeListener(this);
 
+        // add an event listener to the RadioGroup
         numPeopleGroup = findViewById(R.id.numberOfPeopleGroup);
         numPeopleGroup.setOnCheckedChangeListener(this);
 
-        numPeople = 2;
+        numPeople = 2; // what is the better way to get the "default" number of people?
     }
 
 
+    /**
+     * Collects data and updates the display when Views are interacted with
+     */
     public void update(){
         // get the data from the EditText
         billAmountString = billAmountEditText.getText().toString();
@@ -132,15 +133,23 @@ public class MainActivity extends AppCompatActivity
         TextView tipAmountTextView = findViewById(R.id.tipAmount);
         tipAmountTextView.setText(currency.format(tipAmount));
 
+        // update the total amount
         TextView totalAmountTextView = findViewById(R.id.totalAmount);
         totalAmountTextView.setText(currency.format(totalAmount));
 
+        // update the total per person amount
         TextView totalPerPersonTextView = findViewById(R.id.totalPerPersonAmount);
         totalPerPersonTextView.setText(currency.format(totalPerPerson));
     }
 
 
     @Override
+    /**
+     * Listens for the Action Button press and calls the update method.
+     *
+     * @see     android.widget.TextView.OnEditorActionListener
+     * @see     EditorInfo
+     */
     public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
         switch(actionId){
             case EditorInfo.IME_ACTION_DONE:
@@ -153,15 +162,21 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    /**
+     * Listens for click events, only performing a meaningful action when the buttons are clicked.
+     *
+     * @see     android.view.View.OnClickListener
+     */
     public void onClick(View view) {
+        // which View was clicked?
         switch(view.getId()){
             case R.id.tipPercentDec:
-                if(tipPercent > 0) {
+                if(tipPercent > 0) { // only decrease the tip if it's greater than 0
                     tipPercent -= 0.05f;
                 }
                 break;
             case R.id.tipPercentInc:
-                if(tipPercent < 1) {
+                if(tipPercent < 1) { // only increase the tip if it's less  than 1 (100%)
                     tipPercent += 0.05f;
                 }
                 break;
@@ -170,6 +185,7 @@ public class MainActivity extends AppCompatActivity
                 break;
         }
 
+        // re-bind the tip between 0 and 1 (100%)
         if(tipPercent > 1) tipPercent = 1f;
         if(tipPercent < 0) tipPercent = 0f;
 
@@ -180,6 +196,12 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    /**
+     * Listens for SeekBar changes. In this case we aren't interest in the start and stop, only
+     * the continuous update
+     *
+     * @see     android.widget.SeekBar.OnSeekBarChangeListener
+     */
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         if(fromUser){
             tipPercent = progress / 100f;
@@ -189,12 +211,19 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onStartTrackingTouch(SeekBar seekBar) { }
+    public void onStartTrackingTouch(SeekBar seekBar) { /*auto-gen. Nothing to do*/ }
 
     @Override
-    public void onStopTrackingTouch(SeekBar seekBar) { }
+    public void onStopTrackingTouch(SeekBar seekBar) { /*auto-gen. Nothing to do*/ }
 
     @Override
+    /**
+     * Listens for key events. In this case we're only listening for the Enter key for the
+     * EditText
+     *
+     * @see     android.view.View.OnKeyListener
+     * @see     KeyEvent
+     */
     public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
         if(keyCode == KeyEvent.KEYCODE_ENTER){
             InputMethodManager imm = (InputMethodManager) this.getSystemService(this.INPUT_METHOD_SERVICE);
@@ -204,7 +233,13 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    /**
+     * Listens for changes to the RadioGroup
+     *
+     * @see     android.widget.RadioGroup.OnCheckedChangeListener
+     */
     public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+        // when the selection is changed, make sure the section for total per person is visible
         LinearLayout layout = findViewById(R.id.numberOfPeopleLayout);
         layout.setVisibility(View.VISIBLE);
 
@@ -221,7 +256,7 @@ public class MainActivity extends AppCompatActivity
             case R.id.onePersonRB:
             default:
                 numPeople = 1;
-
+                //when only one person is selected, remove the total per person
                 layout.setVisibility(View.GONE);
                 break;
         }
